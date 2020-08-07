7d21c69a0f956834399b36bb31ed81d7875e645f
Sandesh <sandeshpandey98@gmail.com>
2020-08-07 17:00:36 +0545

    CRUD author

--- models/author.js
+++ models/author.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name:{
         type: String,
         required: true
     }
+})
+
+authorSchema.pre('remove', function(next){
+    Book.find({author: this.id}).then(books =>{
+        if (books.length > 0){
+            next(new Error('This author has books still'))
+        }else{
+            next()
+        }
+    }).catch(err => {
+        next(err)
+    })
 })
 
 module.exports = mongoose.model('Author', authorSchema)
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorsRouter = require('./routes/authors')
@@ -16,6 +17,7 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(express.urlencoded({limit: '10mb', extended: true}))
+app.use(methodOverride('_method'))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
